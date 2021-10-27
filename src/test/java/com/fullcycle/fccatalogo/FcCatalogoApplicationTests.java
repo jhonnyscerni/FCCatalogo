26b83c41220ecf1e7d808a192ec96dbce812cbc1
jhonnyscerni <jhonnyscerni@gmail.com>
2021-10-27 10:46:17 -0300

    Commit inicial

---
+++ src/test/java/com/fullcycle/fccatalogo/FcCatalogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullcycle.fccatalogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcCatalogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
